7bc2fabbc4a0f73b46436323b7702f54dd5da12c
sarrthak008 <sarthaknavale08@gmail.com>
2025-03-03 15:09:04 +0530

    add one more brk point to the button

--- src/component/CalcBtn.jsx
+++ src/component/CalcBtn.jsx
@@ -7,11 +7,11 @@ const CalcBtn = ({ sign, type = "primary", logo }) => {
     gray: "bg-gray-400",
     red: "bg-red-700"
   }
-
+  
 
   return (
   
-        <button className={`${BTNS_TYPES[type]}  w-10 sm:w-16 md:w-18 lg:w-20  rounded-xl font-medium text-white h-14 flex items-center justify-center text-center  text-xl cursor-pointer md:text-3xl`}>{sign ? sign : logo}</button> 
+        <button className={`${BTNS_TYPES[type]}  w-10 sm:w-16 md:w-18 lg:w-22 xl:w-24  rounded-xl font-medium text-white h-14 flex items-center justify-center text-center  text-xl cursor-pointer md:text-3xl`}>{sign ? sign : logo}</button> 
 
   )
 }
